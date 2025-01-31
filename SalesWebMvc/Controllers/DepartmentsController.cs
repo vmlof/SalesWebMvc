aed4ac953c89776b0f6819552749190011941c06
Paulo Vitor <Vitormelo.caridade@outlook.com>
2025-01-31 16:49:00 -0300

    first MVC - Department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers;
+
+public class DepartmentsController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        List<Department> departments = new List<Department>();
+        
+        departments.Add(new Department{ Id = 1, Name = "Electronics" });
+        departments.Add(new Department{ Id = 2, Name = "Fashion" });
+        
+        return View(departments);
+    }
+}
